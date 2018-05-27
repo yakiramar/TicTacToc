4f2d49bdbfd4393dd0ac1bdfe9c448eadbb18bf0
yakiramar <33864769+yakiramar@users.noreply.github.com>
2018-05-27 19:35:04 +0300

    Update Champion.h

--- Champion.h
+++ Champion.h
@@ -6,5 +6,5 @@
 class Champion: public Player{
     public:
     const string name()const override;
-    const Coordinate play(const Board& board) override;
+    const Coordinate play(const Board& board) override{return "yakir,naor,dolev";
 };
